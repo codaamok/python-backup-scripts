2b6d12e8fd250154608234816ced5153d41ce80c
CodaAmok <up619480@myport.ac.uk>
2015-07-08 11:57:47 +0100

    Tidied up, added few functions for easier to read code and reduce repetitive code

--- desktopScript-rsync.py
+++ desktopScript-rsync.py
@@ -1,34 +1,44 @@
-# Use this script for the desktop
 import datetime
 import time
 import os
 os.system("cls")
 def main():
-	print " +----------------------------------------------------------------------------+"
-	print " |                               Da Backup Box                                |"
-	print " +----------------------------------------------------------------------------+"
+	lineBreak()
+	print " |                            Raspberry Pi Backup                             |"
+	lineBreak()
 	print " +  Before you backup, it is wise to ensure you're not downloading, uploading +"
-	print " +  or transferring to/from the server. Doing any of those during backup      +"
+	print " +  or transferring to/from the RPi. Doing any of those during backup         +"
 	print " +  will give you a very slow experience. This is because of the bandwidth    +"
 	print " +  limitations that the RPi has when using USB & Ethernet together.          +"
-	print " +----------------------------------------------------------------------------+"
+	lineBreak()
 	x = raw_input("    Do you wish to backup right now? (y/n): ")
 	while x.upper() != "Y" and x.upper() != "N":
-		x = raw_input("    U WOT M8? y or n: ")
-	print " +----------------------------------------------------------------------------+"
+		x = raw_input("    Please enter y or n: ")
+	lineBreak()
 	if x.upper() == "Y":
-		print "    Checking if server is online..."
-		ping = os.system('powershell.exe "Test-Connection -q 192.168.0.5 | Out-Null"') # Success code == 0, error code > 0 Is Out-Null really acceptable?! Does it actually work?!
+		print "    Checking if RPi is online..."
+		ping = testConnection()
 		if ping == 0:
 			print '    Connection OK, initiating backup...'
-			startTime = datetime.datetime.now()
-			command = os.system("rsync -avuih --rsync-path='rsync --log-file=/mnt/disk1/Adam/Dropbox-rsync/rsync.log' --exclude '.dropbox*' --exclude 'desktop.ini' --exclude 'Thumbs.db' --delete --progress --chmod u+rwx -e 'ssh -p 1019 -i /cygdrive/c/Users/Adam/.ssh/id_rsa' '/cygdrive/c/Users/Adam/Dropbox/' username@domain.com::/path/to/directory")
-			finishTime = datetime.datetime.now() - startTime
+			finishTime = rsync()
 		else:
-			print '    Connection failed, quiting...'
+			print "    Can not establish connection to RPi, quitting..."
 	else:
 		print "    OK, will try again tomorrow."
 	print("Total time: " + str(finishTime))
 	time.sleep(5)
-			
+
+def lineBreak():
+	print " +----------------------------------------------------------------------------+"
+
+def testConnection():
+	ping = os.system('powershell.exe "Test-Connection -q 192.168.0.125 | Out-Null"')
+	return ping
+
+def rsync():
+	startTime = datetime.datetime.now()
+	os.system("rsync -rvuih --rsync-path='rsync --log-file=/media/exthdd1/rsync.log' --delete --progress -e 'ssh -p 1019 -i /cygdrive/c/Users/Adam/.ssh/private-passphrase' '/cygdrive/c/Users/Adam/Desktop' pi@192.168.0.125:/media/exthdd1/backup/")
+	finishTime = datetime.datetime.now() - startTime
+	return finishTime
+	
 main()
